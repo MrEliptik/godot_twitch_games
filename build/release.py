cf4dad9743cbe21a7d5f4de4ef7fb5eb5f7e3fc1
mreliptik <victormeunier.dev@gmail.com>
2023-10-30 10:13:30 +0100

    check if tag exists locally or remotely

--- build/release.py
+++ build/release.py
@@ -15,7 +15,7 @@ def parse_build_nb_from_file(file):
 def create_and_push_tag(version):
     # Execute git tag
     cmd = ['git', 'tag', version]
-    print("    |---> Executing command: ", cmd)
+    print("|---> Executing command: ", cmd)
 
     with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, encoding='utf-8') as sp:
         for line in sp.stdout:
@@ -23,15 +23,48 @@ def create_and_push_tag(version):
 
     # Execute git push
     cmd = ['git', 'push', '--tags']
-    print("    |---> Executing command: ", cmd)
+    print("|---> Executing command: ", cmd)
 
     with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, encoding='utf-8') as sp:
         for line in sp.stdout:
             print(line.strip())
 
+def check_existing_tag(version):
+    # Execute git push
+    cmd = ['git', 'tag']
+    print("|---> Executing command: ", cmd)
+
+    local_tags = []
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, encoding='utf-8') as sp:
+        for line in sp.stdout:
+            local_tags.append(line.strip())
+
+
+    cmd = ['git', 'ls-remote', '--tags', 'origin']
+    print("|---> Executing command: ", cmd)
+
+    remote_tags = []
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, encoding='utf-8') as sp:
+        for line in sp.stdout:
+            # Remove all unwanted part of the string to only get the tag name
+            remote_tags.append(line.strip().split("tags/", 1)[1])
+
+    print(remote_tags)
+
+    if version in local_tags:
+        print("    |---> ERROR: Tag already exists locally, stopping release. Tags: ", local_tags)
+        return True
+    if version in remote_tags:
+        print("    |---> ERROR: Tag already exists in origin, stopping release. Tags: ", remote_tags)
+        return True
+    
+    return False
+
 def main():
     version = parse_build_nb_from_file(project_file)
     version = 'v' + version
+
+    if check_existing_tag(version): return
 
     create_and_push_tag(version)
 
