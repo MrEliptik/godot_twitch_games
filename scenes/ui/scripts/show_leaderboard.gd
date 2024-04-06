d2ecb2f7d7f1f27ac657c88b604fd3ec52f2bb07
Container <65561725+TheContainer@users.noreply.github.com>
2024-04-06 15:56:11 +0200

    finished leaderboard logic

--- scenes/ui/scripts/show_leaderboard.gd
+++ scenes/ui/scripts/show_leaderboard.gd
@@ -1,7 +1,21 @@
 extends Control
 
+@onready var leaderboard_tile = load("res://scenes/ui/leaderboard_tile.tscn")
+
 func _ready():
 	show_leaderboard(Leaderboard.list)
 
-func show_leaderboard(data: Dictionary) -> void:
-	pass
+func show_leaderboard(dict: Dictionary) -> void:
+	var keys: Array = dict.keys()
+
+	# Sort keys in descending order of values.
+	keys.sort_custom(func(x: String, y: String) -> bool: return dict[x] > dict[y])
+
+	for k: String in keys:
+		var top_tile = leaderboard_tile.instantiate()
+		get_node("OuterMarginContainer/VBoxContainer/PanelMarginContainer/ContentPanelContainer/VBoxContainer/ListContainer/PanelContainer/HBoxContainer/TopList").add_child(top_tile)
+		top_tile.get_node("Label").text = k
+		
+		var list_tile = leaderboard_tile.instantiate()
+		get_node("OuterMarginContainer/VBoxContainer/PanelMarginContainer/ContentPanelContainer/VBoxContainer/ListContainer/PanelContainer/HBoxContainer/ScoreList").add_child(list_tile)
+		list_tile.get_node("Label").text = str(dict[k])
